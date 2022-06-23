616f5e20177bcec18c26a149c61264500467ff40
abusulaymaan <rustamov.abdurasul@inbox.ru>
2022-06-23 11:20:54 +0500

    new

---
+++ golang/non_divisible_subset.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func nonDivisibleSubset(k int32, s []int32) map[int32]int32 {
+	// Write your code here
+	x := make(map[int32]int32)
+
+	return x
+
+}
+
+func main() {
+	s := []int32{1, 7, 2, 4}
+	k := int32(3)
+
+	fmt.Println(nonDivisibleSubset(k, s))
+}

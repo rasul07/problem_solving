0f5d8b42f44549c743fdd956670346dcf139960e
abusulaymaan <rustamov.abdurasul@inbox.ru>
2022-06-28 10:21:48 +0500

    Taum and bday

---
+++ golang/acm_icpc_team.go
@@ -0,0 +1,14 @@
+package main
+
+// import "fmt"
+
+// func acmTeam(topic []string) []int32 {
+// 	// Write your code here
+
+// }
+
+func main() {
+	// topics := []string{"10101", "11110", "00010"}
+
+	// fmt.Println(acmTeam(topics))
+}

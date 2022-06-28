c7e5224d40ef1191a61246bada7a6eeae3bb4e29
abusulaymaan <rustamov.abdurasul@inbox.ru>
2022-06-28 14:18:32 +0500

    Grading Students and Apple and Orange

---
+++ golang/apple_and_orange.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func countApplesAndOranges(s int32, t int32, a int32, b int32, apples []int32, oranges []int32) {
+	// Write your code here
+	var apple, orange int
+	for _, v := range apples {
+		if s <= a+v && a+v <= t {
+			apple++
+		}
+	}
+	for _, v := range oranges {
+		if s <= b+v && t >= b+v  {
+			fmt.Println(b+v)
+			orange++
+		}
+	}
+	fmt.Println(apple)
+	fmt.Println(orange)
+}
+
+func main() {
+	var (
+		s, t, a, b      int32   = 2, 3, 1, 5
+		apples, oranges []int32 = []int32{-2}, []int32{-1}
+	)
+	countApplesAndOranges(s, t, a, b, apples, oranges)
+}
---
+++ golang/grading_students.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func gradingStudents(grades []int32) []int32 {
+	// Write your code here
+	var (
+		finals []int32
+		next int32
+	)
+	
+	for _, v := range grades {
+		next = 5 - v%5
+		if v >= 38 {
+			if next < 3 {
+				finals = append(finals, next + v)
+			} else {
+				finals = append(finals, v)
+			}
+		} else {
+			finals = append(finals, v)
+		}
+	}
+	return finals
+}
+
+func main() {
+	g := []int32{73, 67, 38, 33}
+
+	fmt.Println(gradingStudents(g))
+}

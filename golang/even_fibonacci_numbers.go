f7ab984f8fbc0a5d7dce59f6af6bf6dfc58053d6
abusulaymaan <rustamov.abdurasul@inbox.ru>
2022-06-29 19:50:53 +0500

    Project Euler Pr1 and Pr2

---
+++ golang/even_fibonacci_numbers.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var sum int64
+	previous, last := 0, 1
+	for i := 0; last <= 4000000; i++ {
+		if last%2 == 0 {
+			sum += int64(last)
+		}
+		previous, last = last, previous + last
+	}
+
+	fmt.Println(sum)
+
+}

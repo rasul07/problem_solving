cc00a17473021853bb331108d5f3a1868660fee0
abu_sulaymaan <rustamov.abdurasul@inbox.ru>
2022-08-11 23:52:03 +0500

    Day of Programmer and HackerRank Problem Solving 4th star

--- golang/day_of_programmer.go
+++ golang/day_of_programmer.go
@@ -1,11 +1,22 @@
 package main
 
-func dayOfProgrammer(year int32) {
-	// Write your code here
+import "fmt"
 
+func dayOfProgrammer(year int32) string {
+	// Write your code here
+	var result string
+	if year == 1918 {
+		result = "28.08.1918"
+	} else if (year%4 == 0 && year%100 != 0) || year%400 == 0 || (year < 1918 && year%4 == 0) {
+		result = fmt.Sprintf("12.09.%v", year)
+	} else {
+		result = fmt.Sprintf("13.09.%v", year)
+	}
+
+	return result
 }
 
 // func main() {
-// 	// y := 1999
-// 	// fmt.Println(dayOfProgrammer(int32(y)))
+// 	y := 1800
+// 	fmt.Println(dayOfProgrammer(int32(y)))
 // }
